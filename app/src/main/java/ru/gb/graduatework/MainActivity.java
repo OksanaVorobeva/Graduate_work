4a99cedfca1ea186925338f41f1f9b37acfc36ed
Oksana <Ksanka82@yandex.ru>
2023-11-27 14:35:54 +0300

    Menu

---
+++ app/src/main/java/ru/gb/graduatework/MainActivity.java
@@ -0,0 +1,42 @@
+package ru.gb.graduatework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonDirectoryFeelings,buttonStartDiary,buttonMyDiary;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        buttonDirectoryFeelings=findViewById(R.id.button_directory_of_feelings);
+        buttonDirectoryFeelings.setOnClickListener(this::onClickDirectory);
+        buttonStartDiary=findViewById(R.id.button_start_a_diary);
+        buttonStartDiary.setOnClickListener(this::onClickBeganDirectory);
+        buttonMyDiary=findViewById(R.id.button_my_diary);
+        buttonMyDiary.setOnClickListener(this::onClickMyDirectory);
+
+
+    }
+
+    private void onClickDirectory(View view){
+
+    }
+
+    private void onClickBeganDirectory(View view){
+
+    }
+
+    private void onClickMyDirectory(View view){
+
+    }
+}
