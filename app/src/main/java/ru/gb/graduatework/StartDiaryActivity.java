dc9223e9990e88532d552de77e63369f7a5a8cdd
Oksana <Ksanka82@yandex.ru>
2023-11-29 22:41:27 +0300

    Changed StartDiaryActivity,activity_start_diary and added TaskOneOneActivity, activiti_task_one_one

--- app/src/main/java/ru/gb/graduatework/StartDiaryActivity.java
+++ app/src/main/java/ru/gb/graduatework/StartDiaryActivity.java
@@ -1,16 +1,34 @@
 package ru.gb.graduatework;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StartDiaryActivity extends AppCompatActivity {
+
+    Button continueTaskOne;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_diary);
+        init();
+    }
+
+    private void init(){
+        continueTaskOne = findViewById(R.id.button_continue_Task1);
+        continueTaskOne.setOnClickListener(this::onClickContinueTaskOne);
 
 
     }
+
+    private void onClickContinueTaskOne(View view){
+        Intent intent = new Intent(this,TaskOneOneActivity.class);
+        startActivity(intent);
+    }
+
 }
