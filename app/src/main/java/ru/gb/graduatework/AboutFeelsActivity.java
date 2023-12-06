aaabd9200b940344620068e4a13b86c4758549e3
Kostya <aktybaeff@yandex.ru>
2023-12-06 16:00:05 +0500

    Добавил отображение детализации чувства в виде таблицы

--- app/src/main/java/ru/gb/graduatework/AboutFeelsActivity.java
+++ app/src/main/java/ru/gb/graduatework/AboutFeelsActivity.java
@@ -2,9 +2,14 @@ package ru.gb.graduatework;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class AboutFeelsActivity extends AppCompatActivity {
 
@@ -17,7 +22,28 @@ public class AboutFeelsActivity extends AppCompatActivity {
 
     private void init(){
         Intent intent = getIntent();
-        int numbersFeels = intent.getIntExtra(ListFeelsActivity.NUMBER_FEELS,-1);
+        String nameFeels = intent.getStringExtra(ListFeelsActivity.NAME_FEELS);
+        parseJson(nameFeels);
+    }
 
+    private void parseJson(String nameFeels) {
+        try {
+            JSONObject jsonObject = new JSONObject(getResources().getString(R.string.about_feels));
+            JSONObject fullInfoForFeels = jsonObject.getJSONObject(nameFeels);
+
+            TextView name = findViewById(R.id.about_feels_name);
+            TextView example = findViewById(R.id.about_feels_example);
+            TextView sensations = findViewById(R.id.about_feels_sensations);
+            TextView thoughts = findViewById(R.id.about_feels_thoughts);
+            TextView behavior = findViewById(R.id.about_feels_behavior);
+
+            name.setText(fullInfoForFeels.getString("name"));
+            example.setText(fullInfoForFeels.getString("example"));
+            sensations.setText(fullInfoForFeels.getString("sensations"));
+            thoughts.setText(fullInfoForFeels.getString("thoughts"));
+            behavior.setText(fullInfoForFeels.getString("behavior"));
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
